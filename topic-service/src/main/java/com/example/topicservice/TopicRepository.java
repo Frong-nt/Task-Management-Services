c61e3b6ccb7e28a5a15390d16504c5ea2d301187
Lachesisss <ktutora@gmail.com>
2019-11-10 03:14:22 +0900

    Clean branch

--- topic-service/src/main/java/com/example/topicservice/TopicRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.topicservice;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import java.util.List;
-
-public interface TopicRepository extends MongoRepository<Topic, String> {
-    Topic findByTaskID(String id);
-}
