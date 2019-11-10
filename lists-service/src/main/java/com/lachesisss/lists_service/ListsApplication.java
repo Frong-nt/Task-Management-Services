61417d310bca18e2be749548bf96fa8f5d88ed94
Lachesisss <ktutora@gmail.com>
2019-11-10 22:49:03 +0900

    Fix lists-service

--- lists-service/src/main/java/com/lachesisss/lists_service/ListsApplication.java
+++ lists-service/src/main/java/com/lachesisss/lists_service/ListsApplication.java
@@ -2,17 +2,16 @@ package com.lachesisss.lists_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Hello world!
  *
  */
 @SpringBootApplication
-@RestController
 public class ListsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ListsApplication.class, args);
 	}
+
 }
