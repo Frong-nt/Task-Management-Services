c6e1e72f13d6140b0f179f7e7ee50b676e45f544
Lachesisss <ktutora@gmail.com>
2019-11-30 17:25:33 +0900

    Add azure-gateway

---
+++ azure-gateway/src/main/java/com/lachesisss/azure_gateway/AzureGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.lachesisss.azure_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzureGatewayApplication.class, args);
+	}
+
+}
